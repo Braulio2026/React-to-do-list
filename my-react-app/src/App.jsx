a06e9536c9c108ad9912252b5ab0bd9b3880b583
braulio ortiz <ortizcastrobrauliojose6@gmail.com>
2025-09-23 11:49:48 -0600

    fix(feature-main): App.jsx imported components vite.svg and  reactLogo to run the website.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import "./App.css"
+// import "./style.css"
 
-export default function app() {
-   return (
+export default function app() { 
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+     e.preventDefault()
+
+     setTodos([
+      ...todos,
+      {id: crypto.randomUUID(), title: newItem, completed:
+        false },
+     ])
+
+     setTodos([
+      ...todos,
+      {id: crypto.randomUUID(), title: newItem, completed:
+        false },
+     ])
+  }
   
-  <>
-<form className="new-item-form">
+   return (
+<>
+<form onSubmit={handleSubmit} className="new-item-form">
    <div className="form-row">
      <label htmlFor="item">New-item</label>
-     <input type="text" id="item" />
+     <input 
+     value={newItem}
+     onChange={e => setNewItem(e.target.value)}
+     type="text" 
+     id="item" 
+     />
    </div>
    <button className="btn">Add</button>
 </form>
@@ -16,7 +43,29 @@ export default function app() {
     <ul className="list">
       <li>
         <label>
+         <input type="checkbox"/>
+         item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
          <input type="checkbox" />
+         item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+         <input type="checkbox" />
+         item 3
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+         <input type="checkbox" />
+         item 4
         </label>
         <button className="btn btn-danger">Delete</button>
       </li>
